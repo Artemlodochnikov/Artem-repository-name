1a7409cf9326bbfb115ee34791719a185cb17e18
Artem <119759144+Artemlodochnikov@users.noreply.github.com>
2022-12-05 18:04:43 +0500

    Add files via upload

---
+++ program_A5.py
@@ -0,0 +1,9 @@
+print('a', 'b', 'c', sep='*')  # 1 строчка
+print('d', 'e', 'f', sep='**', end='')  # 2 строка
+print('g', 'h', 'i', sep='+', end='%')  # 2строка
+print('j', 'k', 'l', sep='-', end='\n')  # 2строка
+print('m', 'n', 'o', sep='/', end='!')  # 3 строка
+print('p', 'q', 'r', sep='1', end='%')  # 3 строка
+print('s', 't', 'u', sep='&', end='\n')  # 3 строка
+print('v', 'w', 'x', sep='%')  # 4 строка
+print('y', 'z', sep='/', end='!')  # 5 строка
